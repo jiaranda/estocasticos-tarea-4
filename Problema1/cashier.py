ae4a0bb2675264064e444cf57ab8df71394ebef4
jiaranda <jiaranda@uc.cl>
2020-06-30 18:10:28 -0400

    product selection and process

--- Problema1/cashier.py
+++ Problema1/cashier.py
@@ -5,10 +5,15 @@ class Cashier:
         self.attention_20_to_22_distr = attention_20_to_22_distr
         self.time_free = 0
 
-    def client_ready_time(self, time):
+    def client_ready_time(self, time, client):
         if 60 * 60 * 10 <= time <= 60 * 60 * 14:
-            return time + self.attention_10_to_14_distr()
+            time_per_product = self.attention_10_to_14_distr()
         elif 60 * 60 * 14 < time <= 60 * 60 * 20:
-            return time + self.attention_14_to_20_distr()
+            time_per_product = self.attention_14_to_20_distr()
         elif 60 * 60 * 20 < time <= 60 * 60 * 22:
-            return time + self.attention_20_to_22_distr()
+            time_per_product = self.attention_20_to_22_distr()
+
+        res = time
+        for _ in range(client.products_qty):
+            res += time_per_product()
+        return res
