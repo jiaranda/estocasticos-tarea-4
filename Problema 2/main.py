3be2dddc40f478ef4d2975cbf7c6862492f6a85e
Javier Aranda Pizarro <jiaranda@uc.cl>
2020-07-02 17:32:20 -0400

    Problem 2 part d

---
+++ Problema 2/main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def instance_generator():
+    def instance():
+        # codigo visto en clases
+        u = np.random.uniform(0, 1)
+        return (u * 7/4) ** (4/7)
+    return instance
+
+
+if __name__ == "__main__":
+    data = list()
+    gen = instance_generator()
+    for i in range(10000):
+        data.append(gen())
+    plt.hist(data, density=True)
+    plt.show()
