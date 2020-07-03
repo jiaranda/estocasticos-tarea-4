8b8866d97eeb22dcb856a6bdd37ed314cee9bd12
Javier Aranda Pizarro <jiaranda@uc.cl>
2020-07-02 20:29:43 -0400

    changed to plotly

--- Problema 2/main.py
+++ Problema 2/main.py
@@ -1,19 +1,25 @@
+import plotly.figure_factory as ff
 import numpy as np
-import matplotlib.pyplot as plt
-
 
 def instance_generator():
     def instance():
         # codigo visto en clases
         u = np.random.uniform(0, 1)
-        return (u * 7/4) ** (4/7)
+        return (u * (7/4)) ** (4/7)
     return instance
 
+x = list()
+gen = instance_generator()
+c = (7/4)**(4/7)
+for i in range(10000):
+    g = gen()
+    x.append(g)
 
-if __name__ == "__main__":
-    data = list()
-    gen = instance_generator()
-    for i in range(100):
-        data.append(gen())
-    plt.hist(data, density=True)
-    plt.show()
+
+hist_data = [x]
+
+
+group_labels = ['distplot'] # name of the dataset
+
+fig = ff.create_distplot(hist_data, group_labels, bin_size=0.02, histnorm='probability')
+fig.show()
